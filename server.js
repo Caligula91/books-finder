fdc914e728270b3e1934df483d2c6fe4773a2be3
Mihajlo Nikolic <nanica71@gmail.com>
2021-01-07 17:02:02 +0100

    Removed manually assigning thread pool size

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-const OS = require('os');
 require('dotenv').config({ path: './config.env' });
-
-process.env.UV_THREADPOOL_SIZE = OS.cpus().length;
-if (process.env.NODE_ENV === 'development')
-  console.log('THREAD POOL SIZE: ', process.env.UV_THREADPOOL_SIZE);
 
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
